a3e094ea97f0528d1b8cf936797a6704cd180fbf
Nemanja <nemanja.todorovicpi@gmail.com>
2020-12-22 23:13:44 +0100

    WRITE funkcija

--- stred.c
+++ stred.c
@@ -9,7 +9,7 @@
 #include <linux/uaccess.h>
 #include <linux/errno.h>
 #include <linux/device.h>
-#define BUFF_SIZE 20
+#define BUFF_SIZE 110
 MODULE_LICENSE("Dual BSD/GPL");
 
 dev_t my_dev_id;
@@ -17,38 +17,38 @@ static struct class *my_class;
 static struct device *my_device;
 static struct cdev *my_cdev;
 
-int lifo[10];
+char string[100];
 int pos = 0;
 int endRead = 0;
 
-int lifo_open(struct inode *pinode, struct file *pfile);
-int lifo_close(struct inode *pinode, struct file *pfile);
-ssize_t lifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
-ssize_t lifo_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset);
+int stred_open(struct inode *pinode, struct file *pfile);
+int stred_close(struct inode *pinode, struct file *pfile);
+ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset);
+ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset);
 
 struct file_operations my_fops =
 {
 	.owner = THIS_MODULE,
-	.open = lifo_open,
-	.read = lifo_read,
-	.write = lifo_write,
-	.release = lifo_close,
+	.open = stred_open,
+	.read = stred_read,
+	.write = stred_write,
+	.release = stred_close,
 };
 
 
-int lifo_open(struct inode *pinode, struct file *pfile) 
+int stred_open(struct inode *pinode, struct file *pfile) 
 {
-		printk(KERN_INFO "Succesfully opened lifo\n");
+		printk(KERN_INFO "Succesfully opened stred\n");
 		return 0;
 }
 
-int lifo_close(struct inode *pinode, struct file *pfile) 
+int stred_close(struct inode *pinode, struct file *pfile) 
 {
-		printk(KERN_INFO "Succesfully closed lifo\n");
+		printk(KERN_INFO "Succesfully closed stred\n");
 		return 0;
 }
 
-ssize_t lifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
+ssize_t stred_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) 
 {
 	int ret;
 	char buff[BUFF_SIZE];
@@ -61,7 +61,7 @@ ssize_t lifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 	if(pos > 0)
 	{
 		pos --;
-		len = scnprintf(buff, BUFF_SIZE, "%d ", lifo[pos]);
+		len = scnprintf(buff, BUFF_SIZE, "%d ", string[pos]);
 		ret = copy_to_user(buffer, buff, len);
 		if(ret)
 			return -EFAULT;
@@ -70,30 +70,83 @@ ssize_t lifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t
 	}
 	else
 	{
-			printk(KERN_WARNING "Lifo is empty\n"); 
+			printk(KERN_WARNING "Stred is empty\n"); 
 	}
 
 	return len;
 }
 
-ssize_t lifo_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
+ssize_t stred_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) 
 {
 	char buff[BUFF_SIZE];
-	int value;
+	int word;
+	int str_len = strlen(string);
 	int ret;
+	int first_char=0;
+	int i=0;
 
 	ret = copy_from_user(buff, buffer, length);
 	if(ret)
 		return -EFAULT;
 	buff[length-1] = '\0';
-
-	if(pos<10)
+	if(!strncmp("string=",buff,7))
+		strcpy(string, *(buff+7));
+	else if(!strncmp("clear",buff,5))
+		for(i = 0; i < 100; i++) *(string+i) = 0;
+	else if(!strcmp("shrink",buff,6))
 	{
-		ret = sscanf(buff,"%d",&value);
-		if(ret==1)//one parameter parsed in sscanf
+		while(buff[i] == ' ') i++;
+		first_char = i;
+		i = length - 1;
+		while(buff[i] == ' ') i--;
+		if(i != length - 1)
+			buff[i] = '\0';
+		for(i = 0; i < 100; i++)
+		{ 
+			if(buff[i] == '\0') break;
+			else string[i] = buff[first_char + 1];
+		}
+	}
+	else if(!strcmp("append=", buff, 7))
+	{
+		if(100 - (length - 1) - str_len > 0)
+			string = strcat(string,*(buff + 7));
+		else
+			printk(KERN_WARNING "APPEND STRING PREDUGACAKKKK\n"); 
+	}
+	else if(!strcmp("truncate=", buff, 9))
+	{
+		ret = sscanf(buff,"%d",&word);
+ 		if( ret != 1 || word < (str_len - 1))
+			printk(KERN_WARNING "Wrong input in function TRUNCATE\n");
+		else
 		{
-			printk(KERN_INFO "Succesfully wrote value %d", value); 
-			lifo[pos] = value; 
+			string[(str_len - 1) - word] = '\0'; 
+			printk(KERN_INFO "Last %d elements are removed.\n",word);
+		}				
+	}
+	/*else if(!strcmp("remove=", buff, 6))
+	{
+		ret = sscanf(buff, *(buff+7), &word);
+		i = ret;
+		if(strstr(word, string) != NULL)
+			while(i < (str_len - 1))
+			{
+				
+				i++;
+			}
+		else printk(KERN_INFO "No matches!\n");
+	}*/	
+
+	else printk(KERN_WARNING "Invalid function call.\n");	
+
+	/*if(pos<100)
+	{
+		//ret = sscanf(buff,"%d",&word);
+		//if(ret==1)//one parameter parsed in sscanf
+		{
+			printk(KERN_INFO "Succesfully wrote value %d", word); 
+			stred[pos] = value; 
 			pos=pos+1;
 		}
 		else
@@ -103,36 +156,37 @@ ssize_t lifo_write(struct file *pfile, const char __user *buffer, size_t length,
 	}
 	else
 	{
-		printk(KERN_WARNING "Lifo is full\n"); 
+		printk(KERN_WARNING "Input string doesn't meet requirements! Max 100 characters.\n"); 
 	}
 
 	return length;
+*/
 }
 
-static int __init lifo_init(void)
+static int __init stred_init(void)
 {
    int ret = 0;
 	int i=0;
 
 	//Initialize array
-	for (i=0; i<10; i++)
-		lifo[i] = 0;
+	for (i=0; i<100; i++)
+		string[i] = 0;
 
-   ret = alloc_chrdev_region(&my_dev_id, 0, 1, "lifo");
+   ret = alloc_chrdev_region(&my_dev_id, 0, 1, "stred");
    if (ret){
       printk(KERN_ERR "failed to register char device\n");
       return ret;
    }
    printk(KERN_INFO "char device region allocated\n");
 
-   my_class = class_create(THIS_MODULE, "lifo_class");
+   my_class = class_create(THIS_MODULE, "stred_class");
    if (my_class == NULL){
       printk(KERN_ERR "failed to create class\n");
       goto fail_0;
    }
    printk(KERN_INFO "class created\n");
    
-   my_device = device_create(my_class, NULL, my_dev_id, NULL, "lifo");
+   my_device = device_create(my_class, NULL, my_dev_id, NULL, "stred");
    if (my_device == NULL){
       printk(KERN_ERR "failed to create device\n");
       goto fail_1;
@@ -149,7 +203,7 @@ static int __init lifo_init(void)
 		goto fail_2;
 	}
    printk(KERN_INFO "cdev added\n");
-   printk(KERN_INFO "Hello world\n");
+   //printk(KERN_INFO "Hello world\n");
 
    return 0;
 
@@ -162,15 +216,15 @@ static int __init lifo_init(void)
    return -1;
 }
 
-static void __exit lifo_exit(void)
+static void __exit stred_exit(void)
 {
    cdev_del(my_cdev);
    device_destroy(my_class, my_dev_id);
    class_destroy(my_class);
    unregister_chrdev_region(my_dev_id,1);
-   printk(KERN_INFO "Goodbye, cruel world\n");
+   //printk(KERN_INFO "Goodbye, cruel world\n");
 }
 
 
-module_init(lifo_init);
-module_exit(lifo_exit);
+module_init(stred_init);
+module_exit(stred_exit);
